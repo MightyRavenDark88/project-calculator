b8cbadbb257a3964cba7be0e09f6f0e13fed81b3
Eric <kinggreenbong@gmail.com>
2024-02-26 18:22:48 -0700

    Add first ad event listener with definitions
    Created ad event listener that tracks button clicks. It then assigns
    numbers into parsed floats, operations into operators, and gets values
    by operation using operator functions.

--- script.js
+++ script.js
@@ -1,12 +1,136 @@
-function add(){
+function add(num1, num2){
+    return parseFloat(num1) + parseFloat(num2);
+}
+function subtract(num1, num2){
+    return parseFloat(num1) - parseFloat(num2);
+}
+function multiply(num1, num2){
+    return parseFloat(num1) * parseFloat(num2);
+}
+function divide(num1, num2){
+    return parseFloat(num1) / parseFloat(num2);
+}
 
-}
-function substract(){
 
-}
-function multiply(){
+let input,
+    result;
 
+//function that prints entries to screen
+function printScreen(input){
+    const screenOutput = document.getElementById('screen');
+    if(screenOutput.textContent.trim() == 0 && input !== "="){
+        screenOutput.textContent = input;
+    }
+    else if(input === "="){
+        screenOutput.textContent = result;
+    }
+    else{
+        screenOutput.textContent += input;
+    }
 }
-function divide(){
-    
-}
+
+//dom reference call for non-number keys
+const buttons = document.querySelectorAll("button"),
+    plusButtonId = document.getElementById("plusButton").id,
+    minusButtonId = document.getElementById("minusButton").id,
+    multiplyButtonId = document.getElementById("multiplyButton").id,
+    divideButtonId = document.getElementById("divideButton").id,
+    equalButtonId = document.getElementById("equalButton").id,
+    cButtonId = document.getElementById("C").id;
+
+let num,
+    fullNum1,
+    fullNum2,
+    operator1,
+    operator2;
+
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        
+        id = event.target.id;
+        input = event.target.textContent;
+        //if number key is pressed
+        if(id !== equalButtonId && id !== plusButtonId && id !== minusButtonId && id !== multiplyButtonId && id !== divideButtonId && id !== cButtonId){
+            //prints button input to screen
+            printScreen(input);
+            
+            //sets current number value
+            if(num === undefined){
+                num = input;
+                console.log("generated original num");
+            }
+            else{
+                num += input;
+                console.log("added number to num");
+            }
+        }
+        //if operator button is pressed
+        else{
+            if(id !== equalButtonId && id !== cButtonId){
+                input = " " + input + " ";
+                printScreen(input);
+                if(fullNum2 === undefined){
+                    fullNum2 = 0;
+                    fullNum1 = parseFloat(num);
+
+                    num = undefined;
+                    console.log("initial");
+                    console.log(fullNum1);
+                    console.log(fullNum2);
+                }
+                //sets current operator
+                switch(id){
+                    case plusButtonId:
+                        operator1 = "+";
+                        console.log("logged add operator")
+                        break;
+                    case minusButtonId:
+                        operator1 = "-";
+                        break;
+                    case multiplyButtonId:
+                        operator1 = "*";
+                        break;
+                    case divideButtonId:
+                        operator1 = "/";
+                        break;
+                    }
+                //sets last operator
+                if(operator2 === undefined){
+                    operator2 = operator1;
+                }
+                else{//calculates result according to non-equal operator
+                        switch(operator2){
+                            case "+":
+                                fullNum2 = add(fullNum2, fullNum1);
+                                console.log("added")
+                                break;
+                            case "-":
+                                fullNum2 = subtract(fullNum2, fullNum1);
+                                console.log("subtracted")
+                                break;
+                            case "*":
+                                fullNum2 = multiply(fullNum2, fullNum1);
+                                console.log("multiplied")
+                                break;
+                            case "/":
+                                fullNum2 = divide(fullNum2, fullNum1);
+                                console.log("divided")
+                                break;
+                        }
+                    operator2 = operator1;
+                    fullNum1 = parseFloat(num);
+                    num = undefined;
+                    console.log("secondary");
+                    console.log(fullNum1);
+                    console.log(fullNum2);
+                }
+            }
+            else if(id !== cButtonId){
+
+            }
+            else{
+
+            }
+        }
+    })
+});
